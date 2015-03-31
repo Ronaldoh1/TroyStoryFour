39a5632b1439140e4e69ffc5a29e02b5be27015f
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-31 12:11:58 -0700

    able to store data and persist - also able to load the information previously stored

--- TroyStoryFour/WarriorListViewController.m
+++ TroyStoryFour/WarriorListViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "WarriorListViewController.h"
+#import "AppDelegate.h"
 
 @interface WarriorListViewController ()<UITableViewDelegate, UITableViewDataSource>
-
+@property NSManagedObjectContext *context;
+@property NSArray *warriors;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -18,19 +21,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    AppDelegate *appDelegate = [[UIApplication sharedApplication]delegate];
+    self.context = appDelegate.managedObjectContext;
+    [self load];
     
+}
+
+-(void)load{
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"Warrior"];
+    self.warriors = [self.context executeFetchRequest:request error:nil];
+    [self.tableView reloadData];
+
+
+
 }
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
 
-    return 1;
+    return self.warriors.count;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
+    NSManagedObject *warrior = [self.warriors objectAtIndex:indexPath.row];
+    cell.textLabel.text = [warrior valueForKey:@"name"];
+    cell.detailTextLabel.text =[[warrior valueForKey:@"prowess"]stringValue];
+    return cell;
 }
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
 
-    return 2;
+
+- (IBAction)saveWarrior:(UITextField *)sender {
+
+    [sender resignFirstResponder];
+    NSManagedObject *newWarrior = [NSEntityDescription insertNewObjectForEntityForName:@"Warrior"inManagedObjectContext:self.context];
+    [newWarrior setValue:sender.text forKey:@"name"];
+    NSNumber *randInt = [NSNumber numberWithLong:arc4random() % 10];
+    [newWarrior setValue:randInt forKey:@"prowess"];
+    [self.context save:nil];
+    [self load];
 }
+
 
 @end
